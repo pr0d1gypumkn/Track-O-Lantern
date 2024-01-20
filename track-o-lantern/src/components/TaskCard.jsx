fc19d33fbd2c8281e12628e3b413467eab946daf
Moe N <moenumasawa@gmail.com>
2024-01-20 04:28:21 -0700

    Create TaskCard component

---
+++ track-o-lantern/src/components/TaskCard.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { TaskContext } from '../pages/Home';
+
+const TaskCard = (props) => {
+    const { taskListData, setTaskListData } = useContext(TaskContext);
+    const {idx, activeIndex, task} = props;
+    const handleCompleteClick = (index) => {
+        const updatedItems = [...taskListData];
+        updatedItems[index].completed = !updatedItems[index].completed;
+        setTaskListData(updatedItems);  
+    };
+    const priorityToBorderColor = {
+        "high": "#f05c44",
+        "medium": "#fb9a05",
+        "low": "#96ad8a"
+    };
+    const borderColor = task.completed ? "#dddddd" : priorityToBorderColor[task.priority];
+
+    return (
+        <div className={idx === activeIndex ? "slide activeSlide" : "slide"} key={idx} style={{borderColor: borderColor}}>
+            <div className="cardContainer">
+            <div className="cardTitle">
+                {task.title}
+            </div>
+            <div className="cardDetails">
+                <div>{task.description}</div>
+                <div>deadline: {task.deadline}</div>
+                <div>priority: {task.priority}</div>
+            </div>
+            <div className="cardComplete">
+                completed: 
+                <input
+                    type="checkbox"
+                    checked={task.completed}
+                    disabled={idx !== activeIndex}
+                    onClick={() => handleCompleteClick(idx)}
+                />
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default TaskCard;
